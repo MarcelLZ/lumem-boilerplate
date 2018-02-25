e195de2a156035a1d47d2f407763d6995389138e
Marcel Zanluca <marcel.zanluca@gmail.com>
2018-02-25 14:43:22 -0300

    Carregando e registrando rotas de forma automática

--- modules/routes.php
+++ modules/routes.php
@@ -1,5 +1,28 @@
 <?php
 
-$router->get('/', function () use ($router) {
-  return 'relo word';
-});
+$diretorioBase = __DIR__;
+$diretorioBaseAberto = opendir($diretorioBase);
+
+while ($diretorio = readdir($diretorioBaseAberto)) {
+  if (ehUmDiretorioValido($diretorioBase, $diretorio)) {
+    carregarModulo([$diretorio, 'module.php'], $router);
+  }
+}
+
+function ehUmDiretorioValido ($diretorioBase, $diretorio) {
+  $diretoriosExcluidos = ['.', '..'];
+
+  return
+    is_dir($diretorioBase . DIRECTORY_SEPARATOR . $diretorio) &&
+    !in_array($diretorio, $diretoriosExcluidos);
+}
+
+function carregarModulo (Array $paths, $router) {
+  try {
+    require_once join(DIRECTORY_SEPARATOR, $paths);
+  } catch (Exception $e) {
+    echo $e->getMessage();
+  }
+}
+
+closedir($diretorioBaseAberto);
